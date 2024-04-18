de2718f3ce9a18f9c39829683028de20c775085a
anouri <ahlem.nouri.gallali@gmail.com>
2024-04-18 05:57:09 +0200

    ex00

---
+++ cell03/ex00/background.js
@@ -0,0 +1,19 @@
+var button = document.getElementById("changeColorButton");
+var fontSize = 16;
+
+
+document.getElementById("changeColorButton").addEventListener("click", function() {
+    var randomColor = getRandomColor();
+    document.body.style.backgroundColor = randomColor;
+    fontSize *=2;
+    button.style.fontSize = fontSize + "px"
+});
+
+function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
