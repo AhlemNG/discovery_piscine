0095e7fd136fed2e53813e13451afb5bffd6bd27
Ahlem NOURI <anouri@student.42.fr>
2024-04-18 10:22:19 +0200

    corrected shell 04

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -2,19 +2,16 @@
 
 # Vérification du nombre d'arguments
 if [ $# -eq 0 ]; then
-    echo "Aucun argument passé au programme."
-elif [ $# -gt 3 ]; then
-    echo "Trop d'arguments passés au programme. Seuls les trois premiers seront affichés."
-else
+    echo "No arguments supplied"
+fi
     # Affichage des arguments
-    echo "Arguments passés au programme :"
     count=1
     for arg in "$@"; do
         if [ $count -le 3 ]; then
-            echo "Argument $count : $arg"
+            echo "$arg"
          ((count++))
         else
             break
         fi
 done
-fi
+
